0de5867247700cfdcce41ef3d1189be5a07f0829
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-11-20 16:12:37 +0200

    all get requst

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,32 @@ var router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
+// read file
+function getFileContent(filePath, req, res) {
+  console.log("filePath: ", filePath);
+  const options = {
+    root: filePath,
+  };
+  try {
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(404).send("User folder not found");
+      }
+      console.log("req.params.item: ", req.params.item);
+      res.sendFile(req.params.item, options, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(404).send("User folder not found");
+        }
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// read folder
 function getFolderContent(folderPath, res) {
   fs.readdir(folderPath, (err, files) => {
     if (err) return res.status(500).send("Error reading folder");
@@ -42,27 +68,7 @@ router.get("/:userName", async (req, res) => {
   });
 });
 
-// read file
-function getFileContent(filePath, req, res) {
-  console.log("filePath: ", filePath);
-  const options = {
-    root: filePath,
-  };
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(404).send("User folder not found");
-    }
-    console.log("req.params.item: ", req.params.item);
-    res.sendFile(req.params.item, options, (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(404).send("User folder not found");
-      }
-    });
-  });
-}
-
+//check if item is folder or file
 router.get("/:userName/:item", async (req, res) => {
   const itemPath = path.join(
     "./",
@@ -76,7 +82,25 @@ router.get("/:userName/:item", async (req, res) => {
     if (err) return res.status(500).send("Error reading folder");
     stats.isDirectory()
       ? getFolderContent(itemPath, res)
-      : getFileContent(userPathz, req, res);
+      : getFileContent(userPath, req, res);
+  });
+});
+
+//read item in the folder
+router.get("/:userName/:folder/:item", async (req, res) => {
+  const nstedFilePath = path.join(
+    "./",
+    "users",
+    req.params.userName,
+    req.params.folder
+  );
+  fs.access(nstedFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).send("User folder not found");
+    }
+
+    getFileContent(nstedFilePath, req, res);
   });
 });
 
