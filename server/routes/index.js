70a3f83f4044afa1f07a310637de68af4f6125d1
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-11-21 10:32:27 +0200

    login

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const userArray = [{ name: "eliyan", password: "1234", id: "1" }];
+router.post("/login", (req, res) => {
+  const user = userArray.find((user) => user.name === req.body.name);
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+  if (user.password !== req.body.password) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+  return res.status(200).json({ user: user, mesage: "loged in sucssefully" });
 });
 
 module.exports = router;
